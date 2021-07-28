22ad0c3e299fc039b731533d58534335fc27ebab
かとぽよ☆ <69085860+shinexkazuma@users.noreply.github.com>
2021-07-28 14:43:46 +0900

    第1章のサンプル☆

---
+++ chap01/list0106.cpp
@@ -0,0 +1,10 @@
+//“ñ‚Â‚Ì®”’l‚P‚W‚Æ‚U‚R‚Ì˜a‚ğ‹‚ß‚Ä•\¦
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "18‚Æ63‚Ì˜a‚Í" << 18 + 63 << "‚Å‚·B\n";
+}
---
+++ chap01/list0119.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name; //名前
+
+	cout << "お名前は : "; //名前の入力を促す
+	getline(cin, name);	   //名前を読み込む（スペースも読み込む）
+
+	cout << "こんにちは！" << name << "さん。\n"; //挨拶する
+}
